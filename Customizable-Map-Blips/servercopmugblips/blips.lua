f971e383bef45e2d87cea10f22a01dc4eb0efda4
ServerCopMug <63811304+ServerCopMug@users.noreply.github.com>
2020-05-06 17:24:54 -0400

    Add files via upload

---
+++ Customizable-Map-Blips/servercopmugblips/blips.lua
@@ -0,0 +1,91 @@
+-----------------------------------------------------
+-----------------------------------------------------
+--------------Made by ServerCopMug#0392--------------
+--------------Customizable Map Blips-----------------
+-----------------------------------------------------
+-----------------------------------------------------
+
+	local blips = {
+		-- The Prison
+	 	{title="Bolingbroke Penitentiary", colour=75, id=238, x=1807.53, y=2605.59, z=45.565},
+		-- Airports 
+		{title="Airport", colour=3, id=90, x=2149.59, y=4817.6, z=41.268},
+		-- Hostipals
+		{title="Hospital", colour=74, id=61, x=337.238, y=-1396.28, z=32.5092},
+		{title="Hospital", colour=74, id=61, x=-450.586, y=-340.387, z=34.5014},
+		{title="Hospital", colour=74, id=61, x=1839.74, y=3672.0, z=34.2767},
+		{title="Hospital", colour=74, id=61, x=360.222, y=-582.222, z=28.8212},
+		{title="Hospital", colour=74, id=61, x=-243.463, y=6327.87, z=32.4262},
+		{title="Hospital", colour=74, id=61, x=-677.003, y=310.929, z=83.0841},
+		{title="Hospital", colour=74, id=61, x=1151.31, y=-1529.95, z=34.9904},
+		{title="Hospital", colour=74, id=61, x=-874.614, y=-308.375, z=39.5448},
+		-- Banks
+		{title="Bank", colour=2, id=431, x=-2964.76, y=482.658, z=15.7068},
+		{title="Bank", colour=2, id=431, x=260.232, y=205.886, z=106.284},
+		{title="Bank", colour=2, id=431, x=150.061, y=-1039.99, z=29.3778},
+		{title="Bank", colour=2, id=431, x=-1213.57, y=-328.829, z=37.7908},
+		{title="Bank", colour=2, id=431, x=-109.453, y=6464.05, z=31.6267},
+		--Race Track
+		{title="Race Track", colour=6, id=147, x=1131.91, y=101.72, z=83.023},
+		{title="Race Track", colour=6, id=147, x=1069.62, y=2362.56, z=43.91},
+		-- Casino
+		{title="Casino", colour=81, id=277, x=930.71, y=41.14, z=78.513},
+		--Interioirs 
+    		{title="Map Interior", colour=39, id=357, x=-936.16,   y=-2986.84,  z=13.95},
+    		{title="Map Interior", colour=39, id=357, x=-1046.61,  y=-2872.69,  z=34.4},
+    		{title="Map Interior", colour=39, id=357, x=128.91,  y=-1299.02,  z=29.23},
+    		{title="Map Interior", colour=39, id=357, x=609.91,  y=-3054.71,  z=5.61},
+    		{title="Map Interior", colour=39, id=357, x=454.32,  y=-3075.56,  z=5.62},
+    		{title="Map Interior", colour=39, id=357, x=-63.14,  y=-2520.47,  z=7.4},
+    		{title="Map Interior", colour=39, id=357, x=11.67,  y=-2652.74,  z=6.01},
+    		{title="Map Interior", colour=39, id=357, x=-63.57,   y=-538.34,  z=31.62},
+    		{title="Map Interior", colour=39, id=357, x=-834.02,  y=165.28,  z=68.83},
+    		{title="Map Interior", colour=39, id=357, x=8.79,  y=540.52,  z=176.03},
+    		{title="Map Interior", colour=39, id=357, x=1394.47,  y=3598.16,  z=34.98},
+    		{title="Map Interior", colour=39, id=357, x=1993.04,  y=3058.28,  z=47.06},
+    		{title="Map Interior", colour=39, id=357, x=-2341.34,  y=3265.45,  z=32.83},
+    		{title="Map Interior", colour=39, id=357, x=2637.69,  y=2935.12,  z=44.7},
+    		{title="Map Interior", colour=39, id=357, x=-596.73,  y=2091.27,  z=131.41},
+    		{title="Map Interior", colour=39, id=357, x=-577.32,  y=5312.67,  z=70.21},
+    		{title="Map Interior", colour=39, id=357, x=-193.75,  y=6150.19,  z=36.44},
+    		{title="Map Interior", colour=39, id=357, x=1614.29,  y=3583.6,  z=35.15},
+    		{title="Map Interior", colour=39, id=357, x=94.34,  y=3744.18,  z=39.72},
+    		{title="Map Interior", colour=39, id=357, x=-759.08,  y=5591.39,  z=36.71},
+    		{title="Map Interior", colour=39, id=357, x=1978.94,  y=3816.49,  z=32.5},
+    		{title="Map Interior", colour=39, id=357, x=-564.69,  y=274.18,  z=83.02},
+    		{title="Map Interior", colour=39, id=357, x=-254.01,  y=-2027.83,  z=29.95},
+    		{title="Map Interior", colour=39, id=357, x=-1044.61,  y=-229.06,  z=39.01},
+    		{title="Map Interior", colour=39, id=357, x=2432.85,  y=4978.43,  z=46.56},
+    		{title="Map Interior", colour=39, id=357, x=-74.33,  y=6272.41,  z=31.37},
+    		{title="Map Interior", colour=39, id=357, x=717.55,  y=-987.16,  z=24.12},
+    		{title="Map Interior", colour=39, id=357, x=-610.52,  y=-1608.24,  z=26.75},
+    		{title="Map Interior", colour=39, id=357, x=3627.68,  y=3756.06,  z=28.52},
+    		{title="Map Interior", colour=39, id=357, x=1084.18,  y=-1974.3,  z=31.01},
+    		{title="Map Interior", colour=39, id=357, x=-582.74,  y=-293.02,  z=35.1},
+    		{title="Map Interior", colour=39, id=357, x=-632.74,  y=-238.11,  z=38.06},
+    		{title="Map Interior", colour=39, id=357, x=104.55,  y=-744.08,  z=45.75},
+    		{title="Map Interior", colour=39, id=357, x=959.18,  y=-2184.94,  z=30.51},
+    		{title="Map Interior", colour=39, id=357, x=134.13,  y=-2203.82,  z=7.36},
+    		{title="Map Interior", colour=39, id=357, x=-1149.07,  y=-1521.73,  z=4.52},
+    		{title="Map Interior", colour=39, id=357, x=-13.85,  y=-1443.09,  z=31.1},
+    		{title="Map Interior", colour=39, id=357, x=1275.68,  y=-1722.95,  z=54.65},
+    		{title="Map Interior", colour=39, id=357, x=2332.62,  y=2576.94,  z=46.67},
+
+		}
+
+Citizen.CreateThread(function()
+
+    for _, info in pairs(blips) do
+      info.blip = AddBlipForCoord(info.x, info.y, info.z)
+      SetBlipSprite(info.blip, info.id)
+      SetBlipDisplay(info.blip, 4)
+      SetBlipScale(info.blip, 0.8)
+      SetBlipColour(info.blip, info.colour)
+      SetBlipAsShortRange(info.blip, true)
+	  BeginTextCommandSetBlipName("STRING")
+      AddTextComponentString(info.title)
+      EndTextCommandSetBlipName(info.blip)
+    end
+end)
+
+
